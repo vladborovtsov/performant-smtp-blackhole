a97eb89360db50b8ed7e550a51d70438fba5cfe3
Vlad Borovtsov <borovcov.vlad@gmail.com>
2023-12-03 16:42:15 +0100

    conditionaly initalize tls identity when SMTPS listeners defined

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use native_tls::Identity;
 use tokio::io::{AsyncBufReadExt, AsyncRead, AsyncWrite};
 use tokio::net::TcpListener as TokioTcpListener;
-use tokio_native_tls::TlsAcceptor as TokioTlsAcceptor;
+use tokio_native_tls::{TlsAcceptor as TokioTlsAcceptor, TlsAcceptor};
 use std::fs::File;
 use std::io::Read;
 use std::net::ToSocketAddrs;
@@ -31,6 +31,7 @@ fn parse_listeners(listeners: Option<&str>) -> Result<Vec<(String, u16)>, Box<dy
 
 }
 
+#[allow(unused_variables)] //TODO: remove this ones function body is implemented.
 async fn handle_client(stream: impl AsyncRead + AsyncWrite + Unpin, is_smtps: bool) -> Result<(), Box<dyn Error + Send + Sync>> {
     // Your code to handle the client connection goes here.
     // For example, you might read from or write to the stream.
@@ -42,13 +43,6 @@ async fn handle_client(stream: impl AsyncRead + AsyncWrite + Unpin, is_smtps: bo
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let cert_path = std::env::var("CERT_PATH").expect("CERT_PATH not set");
-    let mut file = File::open(cert_path)?;
-    let password = std::env::var("CERT_PASSWORD").expect("CERT_PASSWORD not set");
-    let mut identity_bytes = vec![];
-    file.read_to_end(&mut identity_bytes)?;
-    let identity = Identity::from_pkcs12(&identity_bytes, &password)?;
-
     let smtp_listeners_str = std::env::var("SMTP_LISTENERS").ok();
     let smtps_listeners_str = std::env::var("SMTPS_LISTENERS").ok();
 
@@ -57,6 +51,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let smtp_addrs = parse_listeners(smtp_listeners_str.as_deref())?;
     let smtps_addrs = parse_listeners(smtps_listeners_str.as_deref())?;
+
+    let mut tokio_tls_acceptor: Option<Arc<TlsAcceptor>> = None;
+    if !smtps_addrs.is_empty() {
+        let cert_path = std::env::var("CERT_PATH").expect("CERT_PATH not set");
+        let password = std::env::var("CERT_PASSWORD").expect("CERT_PASSWORD not set");
+        let mut file = File::open(cert_path)?;
+        let mut identity_bytes = vec![];
+        file.read_to_end(&mut identity_bytes)?;
+        let identity = Identity::from_pkcs12(&identity_bytes, &password)?;
+        let tls_acceptor = native_tls::TlsAcceptor::builder(identity).build()?;
+        tokio_tls_acceptor = Option::from(Arc::new(TokioTlsAcceptor::from(tls_acceptor)));
+    }
 
     let mut listeners = Vec::new();
 
@@ -76,23 +82,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         exit(1);
     }
 
-
-    let tls_acceptor = native_tls::TlsAcceptor::builder(identity).build()?;
-    let tokio_tls_acceptor = Arc::new(TokioTlsAcceptor::from(tls_acceptor));
-
     let mut futures = Vec::new();
 
     for (listener, is_smtps) in &listeners {
-        let acceptor = tokio_tls_acceptor.clone();
+        let maybe_acceptor = tokio_tls_acceptor.clone();
         let fut = Box::pin(async move {
             match listener.accept().await {
                 Ok((socket, _)) => {
                     if *is_smtps {
-                        let secure_socket = match acceptor.accept(socket).await {
-                            Ok(s) => s,
-                            Err(e) => return Err(Box::new(e) as Box<dyn Error + Send + Sync>),
-                        };
-                        handle_client(secure_socket, true).await
+                        if let Some(acceptor) = maybe_acceptor {
+                            match acceptor.accept(socket).await {
+                                Ok(secure_socket) => {
+                                    handle_client(secure_socket, true).await
+                                },
+                                Err(e) => return Err(Box::new(e) as Box<dyn std::error::Error + Send + Sync>),
+                            }
+                        }
+                        else {
+                            return Err(Box::try_from("About to handle SMTPS socket, but no identity loaded.").unwrap())
+                        }
                     } else {
                         handle_client(socket, false).await
                     }
