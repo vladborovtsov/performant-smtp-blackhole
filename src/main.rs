63d4bcee9c5d4e53a4fed6f381c7280b941c8fce
Vlad Borovtsov <borovcov.vlad@gmail.com>
2023-12-03 13:20:56 +0100

    rename to smtp blackhole; cargo sync

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,36 @@
-use tokio::net::TcpListener;
 use native_tls::Identity;
 use tokio_native_tls::TlsAcceptor as TokioTlsAcceptor;
 use std::fs::File;
 use std::io::Read;
 use tokio::io::{AsyncRead, AsyncWrite};
+use std::net::{TcpListener, ToSocketAddrs};
+use std::process::exit;
+use tokio::net::TcpListener as TokioTcpListener;
+use std::sync::Arc;
+use std::io::Error;
 
 
-async fn handle_client<S>(mut stream: S, is_smtps: bool)
-    where
-        S: AsyncRead + AsyncWrite + Unpin + Send + 'static,
-{
-    // TODO: Implement SMTP protocol handling here.
-    // If STARTTLS command is received and is_smtps is false, upgrade to TLS.
-    stream;
-    is_smtps;
+fn parse_listeners(listeners: &str) -> Result<Vec<(String, u16)>, Box<dyn std::error::Error>> {
+    listeners.split(',')
+        .map(|s| {
+            s.to_socket_addrs()
+                .map_err(|e| e.into()) // Convert to Box<dyn std::error::Error>
+                .and_then(|mut addrs|
+                    addrs.next()
+                        .ok_or_else(|| "Invalid address".into()) // Convert to Box<dyn std::error::Error>
+                        .map(|addr| (addr.ip().to_string(), addr.port()))
+                )
+        })
+        .collect::<Result<Vec<_>, _>>() // Collect into a Result<Vec<(String, u16)>, Box<dyn std::error::Error>>
+}
+
+async fn handle_client(stream: impl AsyncRead + AsyncWrite + Unpin, is_smtps: bool) -> Result<(), Error> {
+    // Your code to handle the client connection goes here.
+    // For example, you might read from or write to the stream.
+
+    // If something goes wrong, return an Error using the `?` operator or `Err(e)`.
+
+    Ok(()) // If everything is fine, return Ok(())
 }
 
 #[tokio::main]
@@ -25,31 +42,54 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     file.read_to_end(&mut identity_bytes)?;
     let identity = Identity::from_pkcs12(&identity_bytes, &password)?;
 
-    let listener_standard = TcpListener::bind("127.0.0.1:25").await?;
-    let listener_standard_alt = TcpListener::bind("127.0.0.1:2525").await?;
-    let listener_default = TcpListener::bind("127.0.0.1:587").await?;
-    let listener_smtps = TcpListener::bind("127.0.0.1:465").await?;
+    let smtp_listeners_str = std::env::var("SMTP_LISTENERS").unwrap_or("".to_string());
+    let smtps_listeners_str = std::env::var("SMTPS_LISTENERS").unwrap_or("".to_string());
+
+    let smtp_addrs = parse_listeners(&smtp_listeners_str)?;
+    let smtps_addrs = parse_listeners(&smtps_listeners_str)?;
+
+    let mut listeners = Vec::new();
+
+    for addr in smtp_addrs {
+        let listener = TokioTcpListener::bind(addr).await?;
+        listeners.push((listener, false));  // false for SMTP
+    }
+
+    for addr in smtps_addrs {
+        let listener = TokioTcpListener::bind(addr).await?;
+        listeners.push((listener, true));  // true for SMTPS
+    }
+
+    if listeners.is_empty() {
+        eprintln!("Error: No SMTP or SMTPS listeners defined.");
+        exit(1);
+    }
+
 
     let tls_acceptor = native_tls::TlsAcceptor::builder(identity).build()?;
-    let tokio_tls_acceptor = TokioTlsAcceptor::from(tls_acceptor);
-
+    let tokio_tls_acceptor = Arc::new(TokioTlsAcceptor::from(tls_acceptor));
 
     loop {
-        tokio::select! {
-            Ok((socket, _)) = listener_standard.accept() => {
-                tokio::spawn(handle_client(socket, false));
-            },
-            Ok((socket, _)) = listener_standard_alt.accept() => {
-                tokio::spawn(handle_client(socket, false));
-            },
-            Ok((socket, _)) = listener_default.accept() => {
-                tokio::spawn(handle_client(socket, false));
-            },
-            Ok((socket, _)) = listener_smtps.accept() => {
-                let acceptor = tokio_tls_acceptor.clone();
-                let secure_socket = acceptor.accept(socket).await?;
-                tokio::spawn(handle_client(secure_socket, true));
-            }
+        for (listener, is_smtps) in &listeners {
+            let acceptor = tokio_tls_acceptor.clone();
+            let listener = listener.clone();
+            let fut = async move {
+                match listener.accept().await {
+                    Ok((socket, _)) => {
+                        if *is_smtps {
+                            let secure_socket = match acceptor.accept(socket).await {
+                                Ok(s) => s,
+                                Err(e) => return Err(e.into()), // Convert to a common error type
+                            };
+                            handle_client(secure_socket, true).await.map_err(|e| e.into())
+                        } else {
+                            handle_client(socket, false).await.map_err(|e| e.into())
+                        }
+                    },
+                    Err(e) => Err(e.into()), // Convert to a common error type
+                }
+            };
+
         }
     }
 }
